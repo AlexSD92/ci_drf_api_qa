e401120fa02c72ed19561107841a26446dab5887
AlexSD92 <alex.spara.dominguez@gmail.com>
2022-08-29 14:15:34 +0100

    Questions app created, included in settings.py and urls.py

--- ci_drf_api_qa/urls.py
+++ ci_drf_api_qa/urls.py
@@ -3,6 +3,9 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
     path('', include('profiles.urls')),
+    path('questions/', include('questions.urls')),
+
     path('api-auth/', include('rest_framework.urls')),
 ]

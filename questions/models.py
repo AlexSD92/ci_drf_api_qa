b8a555155635079cc18f5679661c3d9a70be821a
AlexSD92 <alex.spara.dominguez@gmail.com>
2022-08-29 14:28:18 +0100

    Question model and serializer created, registered in admin view

--- questions/models.py
+++ questions/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Question(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    question = models.CharField(max_length=300, blank=False)
+    detail = models.TextField(blank=False)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created']
+
+    def __str__(self):
+        return self.question
---
+++ questions/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+from .models import Question
+
+
+class QuestionSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username') 
+    is_owner = serializers.SerializerMethodField()
+    profile_id = serializers.ReadOnlyField(source='owner.profile.id')
+
+    def get_is_owner(self, obj):
+        request = self.context['request']
+        return request.user == obj.owner
+
+    class Meta:
+        model = Question
+        fields = ['id', 'owner', 'question', 'detail', 'profile_id', 'created', 'updated']
